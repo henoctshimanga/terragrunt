212d939997f062d4368ecba88abd04f46997a4b6
henoctshimanga <henoctshimanga79@gmail.com>
2025-01-22 12:22:19 -0500

    creaed other vars, and updated subnets

--- infra-modules/vpc/7-variables.tf
+++ infra-modules/vpc/7-variables.tf
@@ -8,3 +8,28 @@ variable "vpc_cidr_block" {
     type        = string
     default     = "10.0.0.0/16"
 }
+
+variable "azs" {
+    description = "Availability Zones for subnets."
+    type        = list(string)
+}
+
+variable "private_subnets" {
+    description = "CIDR ranges for private subnets."
+    type        = list(string)
+}
+variable "public_subnets" {
+    description = "CIDR ranges for public subnets."
+    type        = list(string)
+}
+
+
+variable "private_subnet_tags" {
+    description = "Private subnet tags."
+    type        = map(any)
+}
+
+variable "public_subnet_tags" {
+    description = "Public subnet tags."
+    type        = map(any)
+}
