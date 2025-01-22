3365d8901c58101c8a8f12c58c7e75d0ab56eb4a
henoctshimanga <henoctshimanga79@gmail.com>
2025-01-22 13:53:33 -0500

    main.tf created

---
+++ infra-live-v2/dev/vpc/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+    backend "local" {
+        path = "dev/vpc/terraform.tfstate"
+    }
+}
+
+module "vpc" {
+    source = "../../../infra-modules/vpc"
+
+    env    = "dev"
+    azs    = ["us-east-1a", "us-east-1b"]
+    private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
+    public_subnets = ["10.0.64.0/19", "10.0.96.0/19"]
+
+    private_subnet_tags = {
+        "kubernetes.io/role/internal-elb"    = 1
+        "kubernetes.io/cluster/dev-demo"     = "owned"
+    }
+}
