70cb0f87f76cdf4ddbdeffe65d8209e58675118c
henoctshimanga <henoctshimanga79@gmail.com>
2025-01-22 12:07:19 -0500

    replaced name, and ipadd with appropriate vars

--- infra-modules/vpc/2-igw.tf
+++ infra-modules/vpc/2-igw.tf
@@ -1,7 +1,7 @@
-resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "this" {
+    vpc_id = aws_vpc.this.id
 
     tags = {
-        Name = "dev-igw"
+        Name = "${var.env}-igw"
     }
 }
